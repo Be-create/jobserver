19a02a957b3c534bfd27211abb8a9289154467b0
Benazir Sultana <benazirsultana1998@gmail.com>
2022-10-21 01:57:45 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import cors from "cors"
 let app = express()
 app.use(cors()) 
 app.use(express.json())
+app.get("/",(req,res)=>{
+res.send("server started")
+})
 await connectdatabase()
+
 app.use(authRouter)
 const port = process.env.PORT||8080
 
